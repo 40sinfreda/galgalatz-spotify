8f729d642e8becf079e8180aed763e7475c7700a
40sinfreda <h.israel2008@gmail.com>
2026-04-10 17:35:39 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,22 @@
-import requests
+import requests, base64, os
 
-r = requests.get(
-    'https://glzxml.blob.core.windows.net/dalet/glglz-onair/onair.xml',
-    headers={'User-Agent': 'Mozilla/5.0'}
+cid = os.environ['SPOTIFY_CLIENT_ID']
+cs = os.environ['SPOTIFY_CLIENT_SECRET']
+rt = os.environ['SPOTIFY_REFRESH_TOKEN']
+
+creds = base64.b64encode(f'{cid}:{cs}'.encode()).decode()
+r = requests.post(
+    'https://accounts.spotify.com/api/token',
+    headers={'Authorization': f'Basic {creds}', 'Content-Type': 'application/x-www-form-urlencoded'},
+    data={'grant_type': 'refresh_token', 'refresh_token': rt}
 )
-print(r.status_code)
-print(r.text[:1000])
+token = r.json()['access_token']
+
+playlist_id = os.environ['SPOTIFY_PLAYLIST_ID']
+r2 = requests.post(
+    f'https://api.spotify.com/v1/playlists/{playlist_id}/tracks',
+    headers={'Authorization': f'Bearer {token}', 'Content-Type': 'application/json'},
+    json={'uris': ['spotify:track:4iV5W9uYEdYUVa79Axb7Rh']}
+)
+print(r2.status_code)
+print(r2.text)
