f040fdb8c69fa92ae8d8cbfa0f832da39b300c67
40sinfreda <h.israel2008@gmail.com>
2026-04-10 17:43:51 +0300

    Update test.py

--- test.py
+++ test.py
@@ -10,13 +10,6 @@ r = requests.post(
     headers={'Authorization': f'Basic {creds}', 'Content-Type': 'application/x-www-form-urlencoded'},
     data={'grant_type': 'refresh_token', 'refresh_token': rt}
 )
-token = r.json()['access_token']
-
-playlist_id = os.environ['SPOTIFY_PLAYLIST_ID']
-r2 = requests.post(
-    f'https://api.spotify.com/v1/playlists/{playlist_id}/tracks',
-    headers={'Authorization': f'Bearer {token}', 'Content-Type': 'application/json'},
-    json={'uris': ['spotify:track:4iV5W9uYEdYUVa79Axb7Rh']}
-)
-print(r2.status_code)
-print(r2.text)
+data = r.json()
+print('scope:', data.get('scope'))
+print('token_type:', data.get('token_type'))
