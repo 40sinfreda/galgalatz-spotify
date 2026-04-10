77bf895903c4a067117ba7546ff2110401ee75c3
40sinfreda <h.israel2008@gmail.com>
2026-04-10 17:45:45 +0300

    Update test.py

--- test.py
+++ test.py
@@ -10,6 +10,9 @@ r = requests.post(
     headers={'Authorization': f'Basic {creds}', 'Content-Type': 'application/x-www-form-urlencoded'},
     data={'grant_type': 'refresh_token', 'refresh_token': rt}
 )
-data = r.json()
-print('scope:', data.get('scope'))
-print('token_type:', data.get('token_type'))
+token = r.json()['access_token']
+
+# Get current user ID
+me = requests.get('https://api.spotify.com/v1/me', headers={'Authorization': f'Bearer {token}'})
+print(me.json().get('id'))
+print(me.json().get('display_name'))
